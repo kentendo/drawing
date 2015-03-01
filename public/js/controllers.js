2a8cc4c4c94af85409d7fba5b6a0ea78e3bcb561
Kentendo <kenny@kennycosca.com>
2015-02-28 19:41:30 -0800

    fixed close button and something else

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,13 +1,14 @@
 
 app.controller('DrawingController', ['$scope', function($scope) {
-	$scope.showDebug = true;
+	$scope.showDebug = false;
 	$scope.showControls = true;
 	$scope.size = 11;
 	$scope.zoom = 100;
 	$scope.maxSize = 300;
 	$scope.minSize = 1;
-	$scope.colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "00FFFF"];
+	$scope.colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '00FFFF'];
 	$scope.color = $scope.colors[Math.round(Math.random() * ($scope.colors.length-1))];
+	$scope.brushes = ['round', 'butt', 'square'];
 	$scope.brush = "round";
 	$scope.cursorX = 0;
 	$scope.cursorY = 0;
